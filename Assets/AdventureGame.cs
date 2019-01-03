19c06c54d8e0f64aa4c29f3c880cb38f355d3357
Ryzor0831 <46089652+Ryzor0831@users.noreply.github.com>
2019-01-02 21:11:19 -0500

    Update AdventureGame.cs
    Working on storing states in arrays.

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,6 +24,23 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextState();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            textComponent.text = state.GetStateStory();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            textComponent.text = state.GetStateStory();
+        }
+
     }
 }
